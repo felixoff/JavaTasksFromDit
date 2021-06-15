f4169cf7acd118fb1aca43cf10957c66e77d2ebe
19188656 <NIFeliksov@sberbank.ru>
2021-06-15 17:33:05 +0300

    3tasks

---
+++ Security/src/main/java/org/felix/security/domain/Role.java
@@ -0,0 +1,12 @@
+package org.felix.security.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
