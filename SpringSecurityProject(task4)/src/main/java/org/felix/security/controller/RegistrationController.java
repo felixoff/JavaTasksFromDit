101f704575061365a8c777e51fe46b98a8cec1e0
19188656 <NIFeliksov@sberbank.ru>
2021-06-16 18:23:31 +0300

    tasks1-4

---
+++ SpringSecurityProject(task4)/src/main/java/org/felix/security/controller/RegistrationController.java
@@ -0,0 +1,43 @@
+package org.felix.security.controller;
+
+import org.felix.security.domain.Role;
+import org.felix.security.domain.User;
+import org.felix.security.repos.UserRepo;
+import org.felix.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
+
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
---
+++ SpringSecurityProject(task4)/src/main/java/org/felix/security/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package org.felix.security.repos;
+
+import org.felix.security.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
